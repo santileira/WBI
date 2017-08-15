be7f9aa0904db9281cdfeb905cd57f40ccfaa7d0
santileira <santiagoleira10@gmail.com>
2017-08-15 14:45:48 -0300

    Funcionalidad mail

--- web/js/send_email.php
+++ web/js/send_email.php
@@ -1,17 +1,17 @@
 <?php
-if(isset($_POST["Nombre"]) && isset($_POST["Email"]) && isset($_POST["Mensaje"]) ){
-	$to = "santiagoleira10@gmail.com";
-	$subject = "WEB E-mail Contacto - CABANIAS LA MORENA";
-	$contenido .= "Nombre: ".$_POST["Nombre"]."\n";
-	$contenido .= "Email: ".$_POST["Email"]."\n";
-	$contenido .= "Telefono: ".$_POST["Telefono"]."\n";
-	$contenido .= "Contenido: ".$_POST["Mensaje"]."\n\n";
-	$header = "From: consultas@bifreelancer.com.ar\n";
-	$header .= "Mime-Version: 1.0\n";
-	$header .= "Content-Type: text/plain";
-	if(mail($to, $subject, $contenido ,$header))
-		echo 1;
-	else
-		echo 0;
-}
+	if(isset($_POST["Nombre"]) && isset($_POST["Email"]) && isset($_POST["Mensaje"]) ){
+		$to = "santiagoleira10@gmail.com";
+		$subject = "WEB Contacto";
+		$contenido .= "Nombre: ".$_POST["Nombre"]."\n";
+		$contenido .= "Email: ".$_POST["Email"]."\n";
+		$contenido .= "Telefono: ".$_POST["Telefono"]."\n";
+		$contenido .= "Contenido: ".$_POST["Mensaje"]."\n\n";
+		$header = "From: consultas@bifreelancer.com.ar\n";
+		$header .= "Mime-Version: 1.0\n";
+		$header .= "Content-Type: text/plain";
+		if(mail($to, $subject, $contenido ,$header))
+			echo 1;
+		else
+			echo 0;
+	}
 ?>
